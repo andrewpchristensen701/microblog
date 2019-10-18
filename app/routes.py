2fd675da4b936dff0a1b1cf16e12cf1f4df02505
Andrew Christensen <andrewpchristensen701@gmail.com>
2019-10-18 13:19:57 -0400

    <your_commit_message_here>

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'You'}
+    posts = [
+        {
+            'author': {'username': 'Him'},
+            'body': 'AHHHHHHHHHHHHHH'
+        },
+        {
+            'author': {'username': 'Her'},
+            'body': 'Help me'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
